37dd259d0c1d8dc444819f7369c3425717bbbd2e
Aleksandar Buza <git@aleksandarbuza.com>
2022-10-23 19:08:43 +0200

    chore: add initial version

---
+++ libpunsctl/namespace.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Aleksandar Buza <me@aleksandarbuza.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import os
+from pathlib import Path
+from typing import List, Optional
+
+from .rootspace import RootSpace
+
+__all__ = ["Namespace", "NamespaceException"]
+
+MSG_ANOTHER_NS_ACTIVE = "{name} namespace are active"
+
+
+class NamespaceException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class Namespace(object):
+    def __init__(
+        self,
+        name: str,
+        root_space: RootSpace,
+        symlink_path: Optional[Path] = Path.home(),
+    ):
+        self.name = name
+        self.root_space = root_space
+        self.symlink_path = symlink_path
+
+        self.__path = Path(f"{root_space.get_path()}/{name}")
+
+    def create(self) -> None:
+        try:
+            os.mkdir(self.__path, mode=0o744)
+
+        except OSError as exc:
+            raise NamespaceException(message=exc.strerror)
+
+    def remove(self) -> None:
+        try:
+            if self.exists():
+                os.rmdir(self.__path)
+
+        except OSError as exc:
+            raise NamespaceException(message=exc.strerror)
+
+    def exists(self) -> bool:
+        if not Path(self.__path).exists():
+            return False
+
+        return True
+
+    def get_path(self) -> Path:
+        return self.__path
+
+    def active(self) -> bool:
+        path = Path(f"{self.root_space.path}/.active")
+
+        if path.exists() and Path(os.readlink(path)) == self.__path:
+            return True
+
+        return False
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_namespace_path(self) -> Path:
+        return self.__path
+
+    def __get_sources(self) -> List[Path]:
+        return [source for source in self.__path.iterdir()]
+
+    def activate(self) -> None:
+        path = Path(f"{self.root_space.path}/.active")
+
+        if path.exists() and path.is_symlink():
+            if Path(os.readlink(path)) == self.__path:
+                pass
+
+            else:
+                raise NamespaceException(
+                    message=MSG_ANOTHER_NS_ACTIVE.format(
+                        name=self.root_space.get_active_namespace()
+                    )
+                )
+        else:
+            path.symlink_to(self.__path)
+
+        for source in self.__get_sources():
+            with Path(f"{self.symlink_path}/{source.name}") as target:
+                if target.exists():
+                    if target.is_symlink() and Path(os.readlink(target)) == source:
+                        continue
+
+                    with target.with_name(f"{target.name}.{self.name}.bak") as backup:
+                        if not target.is_symlink():
+                            target.rename(backup)
+
+                if not target.is_symlink():
+                    target.symlink_to(source)
+
+    def deactivate(self) -> None:
+        path = Path(f"{self.root_space.path}/.active")
+
+        if path.exists() and path.is_symlink():
+            if Path(os.readlink(path)) == self.__path:
+                path.unlink()
+
+        for source in self.__get_sources():
+            with Path(f"{self.symlink_path}/{source.name}") as target:
+                if target.exists():
+                    if target.is_symlink() and Path(os.readlink(target)) == source:
+                        target.unlink()
+
+                    with target.with_name(f"{target.name}.{self.name}.bak") as backup:
+                        if backup.exists():
+                            backup.rename(target)
---
+++ punsctl.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Aleksandar Buza <me@aleksandarbuza.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import sys
+from pathlib import Path
+from typing import List, Tuple
+
+from libpunsctl.namespace import Namespace, NamespaceException
+from libpunsctl.rootspace import RootSpace, RootSpaceException
+from libpunsctl.sgetopt import sgetopt
+
+__version__ = "0.0.1"
+
+USAGE = """
+punsctl <options>
+
+options:
+    -h                  Help menu
+    -p                  Root path
+    -l                  List namespaces
+    -c <namespace>      Create namespace
+    -r <namespace>      Remove namespace
+    -a <namespace>      Activate namespace
+    -d                  Deactivate namespaces
+"""
+
+MSG_LIST_NS = "{name} ({path}) {active}"
+
+
+def usage():
+    sys.exit(USAGE)
+
+
+@sgetopt(args=sys.argv[1:], optstring="hplc:r:a:d")
+def main(opts: List[Tuple], argv: List[str]) -> None:
+    if len(opts) == 0:
+        usage()
+
+    opt_root_path = None
+    opt_list = False
+    opt_create = None
+    opt_remove = None
+    opt_activate = None
+    opt_deactivate = False
+
+    for opt, arg in opts:
+        if opt == "-h":
+            usage()
+
+        elif opt == "-p":
+            opt_root_path = arg
+
+        elif opt == "-l":
+            opt_list = True
+
+        elif opt == "-c":
+            opt_create = arg if arg is not None else usage()
+
+        elif opt == "-r":
+            opt_remove = arg if arg is not None else usage()
+
+        elif opt == "-a":
+            opt_activate = arg if arg is not None else usage()
+
+        elif opt == "-d":
+            opt_deactivate = True
+
+        else:
+            usage()
+
+    if opt_root_path is not None:
+        try:
+            root_space = RootSpace(path=Path(opt_root_path))
+
+        except RootSpaceException as exc:
+            print(exc.message)
+            sys.exit()
+
+    else:
+        root_space = RootSpace(path=Path(f"{Path.home()}/.ns"))
+
+    if opt_list:
+        for namespace in root_space.get_namespace_paths():
+            ns = Namespace(root_space=root_space, name=namespace.name)
+
+            print(
+                MSG_LIST_NS.format(
+                    name=namespace.name,
+                    path=namespace.absolute(),
+                    active="active" if ns.active() else "",
+                )
+            )
+
+    elif opt_deactivate:
+        for namespace in root_space.get_namespace_paths():
+            ns = Namespace(root_space=root_space, name=namespace.name)
+            ns.deactivate()
+
+        print("namespaces are deactivated successfully")
+
+    elif opt_create is not None:
+        ns = Namespace(root_space=root_space, name=opt_create)
+
+        try:
+            ns.create()
+            print(f"{opt_create} created")
+
+        except NamespaceException as exc:
+            print(f"error: {exc.message}")
+
+    elif opt_remove is not None:
+        ns = Namespace(root_space=root_space, name=opt_remove)
+
+        try:
+            ns.remove()
+
+        except NamespaceException as exc:
+            print(f"error: {exc.message}")
+
+    elif opt_activate is not None:
+        ns = Namespace(root_space=root_space, name=opt_activate)
+
+        try:
+            ns.activate()
+            print(f"{opt_activate} activated")
+
+        except NamespaceException as exc:
+            print(f"error: {exc.message}")
+
+    else:
+        usage()
+
+
+if __name__ == "__main__":
+    main()
