5cba0ba8bd0f20139de12760cd5c3f611d256f7f
Aleksandar Buza <git@aleksandarbuza.com>
2022-10-26 23:35:35 +0200

    test: add tests

---
+++ tests/test_multiple_namespaces.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Aleksandar Buza <me@aleksandarbuza.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from pathlib import Path
+
+import pytest
+
+from punsctl.namespace import Namespace
+from punsctl.rootspace import RootSpace
+
+NAMESPACES = [
+    "test",
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+    "test6",
+    "test7",
+    "test8",
+    "test9",
+    "test0",
+]
+
+
+@pytest.fixture
+def root_tmpdir(tmpdir):
+    path = Path(f"{tmpdir}/.ns")
+    path.mkdir(parents=True, exist_ok=True)
+
+    return path
+
+
+@pytest.fixture
+def symlink_tmpdir(tmpdir):
+    path = Path(f"{tmpdir}/workspace")
+    path.mkdir(parents=True, exist_ok=True)
+
+    return path
+
+
+def test_multiple_namespace(root_tmpdir, symlink_tmpdir):
+    rs = RootSpace(path=root_tmpdir, symlink_path=symlink_tmpdir)
+
+    for namespace in NAMESPACES:
+        ns = Namespace(root_space=rs, name=namespace)
+
+        assert ns.exists() is False
+
+        ns.create()
+        ns.activate()
+
+        assert ns.active() is True
+        assert ns.get_path() == Path(f"{root_tmpdir}/{ns.get_name()}")
+
+        assert Path(f"{rs.get_symlink_path()}/.current_ns").is_dir() is True
+        assert Path(f"{rs.get_symlink_path()}/.current_ns").is_symlink() is True
+
+        ns.deactivate()
+        ns.remove()
+
+        assert ns.exists() is False
---
+++ tests/test_namespace_exceptions.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Aleksandar Buza <me@aleksandarbuza.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from pathlib import Path
+
+import pytest
+
+from punsctl.namespace import Namespace, NamespaceException
+from punsctl.rootspace import RootSpace
+
+
+@pytest.fixture
+def root_tmpdir(tmpdir):
+    path = Path(f"{tmpdir}/.ns")
+    path.mkdir(parents=True, exist_ok=True)
+
+    return path
+
+
+@pytest.fixture
+def symlink_tmpdir(tmpdir):
+    path = Path(f"{tmpdir}/workspace")
+    path.mkdir(parents=True, exist_ok=True)
+
+    return path
+
+
+@pytest.mark.usefixtures()
+def test_namespace_exception(root_tmpdir, symlink_tmpdir):
+    rs = RootSpace(path=root_tmpdir, symlink_path=symlink_tmpdir)
+    ns1 = Namespace(root_space=rs, name="test_namespace_1")
+    ns2 = Namespace(root_space=rs, name="test_namespace_2")
+
+    assert ns1.exists() is False
+
+    ns1.create()
+    ns1.remove()
+
+    with pytest.raises(NamespaceException):
+        ns1.activate()
+
+    ns1.create()
+    ns1.activate()
+
+    with pytest.raises(NamespaceException):
+        ns2.activate()
